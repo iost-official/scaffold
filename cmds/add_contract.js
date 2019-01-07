b4358e5104be4f37515af45858c924d5b3b97561
aladdin <aladdin@ddex.io>
2019-01-07 18:01:17 +0800

    Migration From EVEREST1.0: change BlockChain to blockchain

--- cmds/add_contract.js
+++ cmds/add_contract.js
@@ -26,7 +26,7 @@ exports.handler = function (argv) {
     var content = "const rstorage = require('../libjs/storage.js');" + "\n" +
         "const rBlockChain = require('../libjs/blockchain.js');" + "\n" +
         "const storage = new rstorage();" + "\n" +
-        "const BlockChain = new rBlockChain();" + "\n\n" +
+        "const blockchain = new rBlockChain();" + "\n\n" +
         "class ContractName" + "\n" +
         "{" + "\n" +
         "    constructor() {" + "\n" +
